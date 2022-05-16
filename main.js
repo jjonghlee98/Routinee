f920ff7567a6c83d66e464da18309971db29fcfe
Jonghwan Lee <jjonghlee98@gmail.com>
2022-05-16 21:08:52 +0900

    list update

--- main.js
+++ main.js
@@ -2,18 +2,49 @@ let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
 
-// btn.onclick = () => {
-//   sidebar.classList.toggle("active");
-// }
-
-// searchBtn.onclick = () => {
-//   sidebar.classList.toggle("active");
-// }
-
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
 
 searchBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
-})
+});
+
+let ddayBtn = document.querySelector(".bx-alarm-add");
+let box2Flex = document.querySelector(".box2_flex");
+let goalBtn = document.querySelector(".bx-add-to-queue");
+let box3Flex = document.querySelector(".box3_flex");
+let dBtn = document.querySelector("#dday_btn");
+let gBtn = document.querySelector("#goal_btn");
+
+ddayBtn.addEventListener("click", () => {
+  box2Flex.classList.toggle("active");
+});
+
+dBtn.addEventListener("click", () => {
+  box2Flex.classList.toggle("active");
+});
+
+goalBtn.addEventListener("click", () => {
+  box3Flex.classList.toggle("active");
+});
+
+gBtn.addEventListener("click", () => {
+  box3Flex.classList.toggle("active");
+});
+
+// const calc = () => {
+//   var now = new Date($("#start").val());
+//   var then = new Date($("#end").val());
+//   var gap = now.getTime() - then.getTime();
+//   gap = Math.floor(gap / (1000 * 60 * 60 * 24));
+
+//   if (gap < 0)
+//   {
+//       $("#output").text(("D " + gap));
+//   }
+//   else
+//   {
+//       $("#output").text(("D + " + gap));
+//   }
+// }
